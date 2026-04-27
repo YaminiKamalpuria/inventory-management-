c6fc46fb474e78e3506a8af87109685f70ce77df
YaminiKamalpuria <yaminikamalpuria@gmail.com>
2026-04-27 23:09:18 +0530

    fix: add vercel URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://inventory-management-nhyt.onrender.com'
+    'https://inventory-management-nhyt.onrender.com',
+    'https://inventory-management-dun-nu.vercel.app'
   ],
   credentials: true
 }));app.use(express.json());
